55fff5b1f131d63d752c12cec28ea02c7c14c9e3
Juanjo Martinez <juanjo.marcal@gmail.com>
2017-06-10 20:03:16 +0200

    Settings added

--- index.php
+++ index.php
@@ -4,21 +4,17 @@
   </head>
   <body>
     <?php
-      ini_set('display_errors', 1);
-      ini_set('display_startup_errors', 1);
-      error_reporting(E_ALL);
-
-      $username = 'lavanderia';
-      $password = 'lavanderia';
-      $database = 'lavanderia';
+      include_once('settings.php');
 
       $conn = new mysqli('mysql', $username, $password, $database);
+
       if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
       }
 
       $sql = 'SELECT * FROM users';
       $result = $conn->query($sql);
+
       if($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
           echo '<div>'.$row['username'].' - '.$row['email'].'</div>';
@@ -26,6 +22,7 @@
       }else{
         echo 'No ha encontrado usuarios';
       }
+
       $conn->close();
     ?>
   </body>
