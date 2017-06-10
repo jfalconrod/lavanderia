4a7398b87d157413dacd3f1bdfb856d49630ed6c
Unknown <jfalcon87@gmail.com>
2017-06-10 22:23:31 +0200

    Auto stash before merge of "master" and "origin/master"

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
   </head>
   <body>
     <?php
@@ -12,12 +15,12 @@
         die("Connection failed: " . $conn->connect_error);
       }
 
-      $sql = 'SELECT * FROM users';
+      $sql = 'SELECT * FROM clientes';
       $result = $conn->query($sql);
 
       if($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-          echo '<div>'.$row['username'].' - '.$row['email'].'</div>';
+          echo '<div>'.$row['codigoCliente'].' - '.$row['nombreCliente'].'</div>';
         }
       }else{
         echo 'No ha encontrado usuarios';
@@ -25,5 +28,6 @@
 
       $conn->close();
     ?>
+    <h1>Falcón</h1>
   </body>
 </html>
