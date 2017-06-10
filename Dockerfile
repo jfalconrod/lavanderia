75d64164c46035bf5f8335893d342c5d1083fbff
Juanjo Martinez <juanjo.marcal@gmail.com>
2017-06-10 19:30:46 +0200

    MySQL added

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM debian:latest
 
 RUN apt-get update && apt-get dist-upgrade -y
-RUN apt-get install nginx php5-fpm vim -y
+RUN apt-get install nginx php5-fpm php5-mysql vim -y
 
 COPY default /etc/nginx/sites-enabled/
--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
-  $test = ['Prueba1', 'Prueba2', 'Prueba3'];
+  $username = 'lavanderia';
+  $password = 'lavanderia';
 
-  foreach($test as $a){
-    echo $a.'<br/s>';
+  $conn = new mysqli('mysql', $username, $password);
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $sql = 'SELECT * FROM users';
+  $result = $conn->query($sql);
+  if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+      echo '<div>'.$row[0].'</div>';
+    }
+  }else{
+    echo 'No ha encontrado usuarios';
   }
 ?>
