67d21ec19285e575b7f26c26bc32cce599b15b94
Juanjo Martinez <juanjo.marcal@gmail.com>
2017-06-10 19:47:13 +0200

    MySQL connections example

--- index.php
+++ index.php
@@ -1,19 +1,32 @@
-<?php
-  $username = 'lavanderia';
-  $password = 'lavanderia';
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <?php
+      ini_set('display_errors', 1);
+      ini_set('display_startup_errors', 1);
+      error_reporting(E_ALL);
 
-  $conn = new mysqli('mysql', $username, $password);
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
+      $username = 'lavanderia';
+      $password = 'lavanderia';
+      $database = 'lavanderia';
 
-  $sql = 'SELECT * FROM users';
-  $result = $conn->query($sql);
-  if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-      echo '<div>'.$row[0].'</div>';
-    }
-  }else{
-    echo 'No ha encontrado usuarios';
-  }
-?>
+      $conn = new mysqli('mysql', $username, $password, $database);
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+
+      $sql = 'SELECT * FROM users';
+      $result = $conn->query($sql);
+      if($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+          echo '<div>'.$row['username'].' - '.$row['email'].'</div>';
+        }
+      }else{
+        echo 'No ha encontrado usuarios';
+      }
+      $conn->close();
+    ?>
+  </body>
+</html>
